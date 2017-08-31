76ee0fff5d079055b15f26b04733708673294d01
tyler58546 <tyler@kk2.com>
2017-08-30 19:39:19 -0700

    Initial Commit

---
+++ Annoyer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Annoyer
+//
+//  Created by Tyler Knox on 8/30/17.
+//  Copyright © 2017 tyler58546. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Annoyer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Annoyer
+//
+//  Created by Tyler Knox on 8/30/17.
+//  Copyright © 2017 tyler58546. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
