833806cfd0bda77bbc4b531767e08e9ec7aebb80
tyler58546 <tyler@kk2.com>
2017-08-31 18:30:06 -0700

    (First Release)

--- Annoyer/ViewController.swift
+++ Annoyer/ViewController.swift
@@ -9,11 +9,39 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var ttsField: NSTextField!
+    @IBOutlet weak var voiceSelector: NSPopUpButton!
+    
+    let loop = RunLoop.current
+    let synth = NSSpeechSynthesizer()
+    func speech(text: String) {
+        synth.startSpeaking(text)
+        let mode = loop.currentMode ?? RunLoopMode.defaultRunLoopMode
+        while loop.run(mode: mode, before: NSDate(timeIntervalSinceNow: 0.1) as Date)
+            && synth.isSpeaking {}
+    }
+    @IBAction func valueChanged(_ sender: NSPopUpButton) {
+        let newVoice = voiceSelector.selectedItem?.title
+        
+        
+        for v in NSSpeechSynthesizer.availableVoices() {
+            let attrs = NSSpeechSynthesizer.attributes(forVoice: v)
+            if attrs["VoiceName"] as? String == newVoice {
+                synth.setVoice(v)
+                break
+            }
+        }
+    }
+    @IBAction func playButtonPressed(_ sender: Any) {
+        speech(text: ttsField.stringValue)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        speech(text: "hi")
     }
 
     override var representedObject: Any? {
---
+++ Annoyer/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Annoyer
+//
+//  Created by Tyler Knox on 8/31/17.
+//  Copyright © 2017 tyler58546. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController2: NSViewController {
+    @IBOutlet weak var pwField: NSSecureTextField!
+
+    @IBAction func quitButtonPressed(_ sender: Any) {
+        NSApplication.shared().terminate(self)
+    }
+    @IBAction func unlockButtonPressed(_ sender: Any) {
+        let pwEntered = pwField.stringValue
+        if (pwEntered == "passw0rd") {
+            //Unlock Application
+            dismiss(true)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
